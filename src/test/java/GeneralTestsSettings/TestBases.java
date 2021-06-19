b03c1fe5988aa32c97ab4250c7da65637dbc3bec
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-06-19 11:36:05 -0400

    Add Class to hide sensitive information

--- src/test/java/GeneralTestsSettings/TestBases.java
+++ src/test/java/GeneralTestsSettings/TestBases.java
@@ -1,14 +1,16 @@
 package GeneralTestsSettings;
 
 import api.ApiRequest;
+import GeneralInfoManagement.InfoManager;
 import org.testng.annotations.BeforeTest;
 
 public class TestBases {
     public static ApiRequest apiRequest = new ApiRequest();
+    InfoManager infoManager = new InfoManager();
     @BeforeTest
     public void setGeneralConfig() {
-        apiRequest.addHeader("X-TrackerToken", "599e3817e376dc26345552c4aa198143");
-        apiRequest.setBaseUri("https://www.pivotaltracker.com/services/v5");
+        apiRequest.addHeader("X-TrackerToken", infoManager.getConfig().getProperty("TOKEN"));
+        apiRequest.setBaseUri(infoManager.getConfig().getProperty("BASE_URI"));
     }
 
 }
